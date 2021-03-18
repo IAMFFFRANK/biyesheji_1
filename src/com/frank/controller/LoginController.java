9d91bcc624f795fc2cbac0b4be7d5f08b26fdf88
IAMFFFRANK <318522639@qq.com>
2021-03-18 23:21:49 +0800

    Graduation project

--- src/com/frank/controller/LoginController.java
+++ src/com/frank/controller/LoginController.java
@@ -1,28 +1,21 @@
 package com.frank.controller;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.frank.entity.Login;
 import com.frank.entity.Result;
 import com.frank.service.ILoginService;
-
 @Controller
 @ResponseBody
 public class LoginController {
-	
 	@Autowired
 	private ILoginService loginServiceImpl;
-	
 	@RequestMapping("login")
 	public List login(HttpServletRequest request, @RequestBody Login login) {
 		List list=loginServiceImpl.login(login);
@@ -33,7 +26,6 @@ public class LoginController {
 		}
 		return list;
 	}
-	
 	@RequestMapping("getSession")
 	public List getSession(HttpServletRequest request,@RequestBody Login login){
 		System.out.println(login);
@@ -42,7 +34,6 @@ public class LoginController {
 		list.add(request.getSession().getAttribute(login.getRole()));
 		return list;
 	}
-	
 	@RequestMapping("exitSys")
 	public Result exitSys(HttpServletRequest request) {
 		String exit="";
